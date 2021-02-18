be6421d1771a3ae31463806f217c4367b8a6cccf
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-02-18 16:18:33 +0700

    Finished Lucky Number

--- Lucky Number.cpp
+++ Lucky Number.cpp
@@ -1,5 +1,44 @@
 #include <stdio.h>
 int main ()
 {
+	int num, i, j, check = 0, temp;
+	scanf("%d", &num);
+	temp = num;
 	
+	for(i=2; check<3; i++)
+	{
+		if(num/i != 1 || check == 2)
+		{
+			if(num%i == 0)
+			{
+				if(i == j)
+				{
+					printf("%d is not a Lucky Number.", temp);
+					return 0;
+				}
+				num = num/i;
+				printf("%d\n", num);
+				j = i;
+				printf("j = %d\n", j);
+				check++;
+				for(i=2; i<j; i++)
+				{
+					if(j%i == 0)
+					{
+						printf("%d is not a Lucky Number.", temp);
+						return 0;
+					}
+				}
+				i = 1;
+			}
+		}
+	}
+	if(num != 1)
+	{
+		printf("%d is not a Lucky Number.", temp);
+		return 0;
+	} else {
+		printf("%d is a Lucky Number.", temp);
+		return 0;
+	}
 }
